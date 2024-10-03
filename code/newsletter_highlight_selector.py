666b67793bc0e5ee47eb842214fa192dfea4cb45
Melanie Moy <melanie.moy@gmail.com>
2024-10-03 13:11:17 -0500

    Script to select a D360 member to highlight in the newsletter

---
+++ code/newsletter_highlight_selector.py
@@ -0,0 +1,44 @@
+import random
+
+D360_TEAM_MEMBERS = [
+    "Juan Garcia Bazan",
+    "Aaron Niskode-Dossett",
+    "Melanie Moy",
+    "Damani Philip",
+    "Edgar Arenas",
+    "Eileen Toomer",
+    "Jeremy Pharo",
+    "Yong-Jin Lee",
+    "Juan Bages",
+    "Eve Ahearn",
+    "Brian McGonigle",
+    "Crist Scheye-Grover",
+    "Edwin Jimenez",
+    "Jorge Banuelos",
+    "Kelly Zhou",
+    "Jeena Lee",
+    "Monica Senapati",
+    "Rachel Hong",
+    "Ryan Bertsche",
+    "Vishal Shah",
+    "Discovery - TBH"
+     ]
+ALREADY_HIGHLIGHTED = []
+
+# Function to get a random value from A that's not in B
+def select_newsletter_highlight():
+    # Create a new list of elements in A that are not in B
+    eligible = [elt for elt in D360_TEAM_MEMBERS if elt not in ALREADY_HIGHLIGHTED]
+
+    # Check if there are any elements left in eligible
+    if not eligible:
+        raise Exception("No more eligible team members to highlight.  Reset the list!")
+
+    # Randomly select and return an element from eligible
+    return random.choice(eligible)
+
+
+# Example usage
+selected_value = select_newsletter_highlight()
+
+print(f'This week\'s highlighted team member: {selected_value}')
