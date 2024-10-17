fa0ab7d17a8406e059bbbb01be46a9cf8b25b5c1
Melanie Moy <melanie.moy@gmail.com>
2024-10-17 10:26:18 -0500

    Update newsletter_highlight_selector.py
    Updating already highlighted to incl. Kelly Zhou

--- code/newsletter_highlight_selector.py
+++ code/newsletter_highlight_selector.py
@@ -23,7 +23,7 @@ D360_TEAM_MEMBERS = [
     "Vishal Shah",
     "Discovery - TBH"
      ]
-ALREADY_HIGHLIGHTED = []
+ALREADY_HIGHLIGHTED = ["Kelly Zhou"]
 
 # Function to get a random value from A that's not in B
 def select_newsletter_highlight():
